350328de0f3ba95979f8b6534051b3f980f6851c
Veronika <koshachina@yahoo.com>
2018-12-11 11:59:49 +0200

    Version 1

---
+++ lesson1.js
@@ -0,0 +1,28 @@
+var budget = +prompt ("Ваш бюджет?", "");
+var name = prompt ("Название вашего магазина?", "");
+
+var mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+}
+
+
+
+
+prompt ("Какой тип1 товаров будем продавать?"),
+    prompt ("Какой тип2 товаров будем продавать?"),
+    prompt ("Какой тип3 товаров будем продавать?")
+
+var OneDayBudget = budget /30;
+alert (OneDayBudget);
+
+
+console.log (budget);
+console.log (name);
+console.log (shopGoods);
+
+
+
---
+++ lesson2.js
@@ -0,0 +1,10 @@
+
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+alert (week []);
+/*
+let arr = ['035468465', '305465460', '985062354', '732516546816','654651321', '35435416846', '7564546545'];
+if (){
+    console.log (arr[])
+};
+
+*/
